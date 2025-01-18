281f2b2e5a11cb1134d57e13786c39c31eb8bb18
Matvei <khlestov.matvei@mail.ru>
2025-01-18 20:17:58 +0300

    Registered simple tag for categories

--- textura_store/main/views.py
+++ textura_store/main/views.py
@@ -1,23 +1,21 @@
 from django.http import HttpResponse
+
 from django.shortcuts import render
 
-from goods.models import Categories
 
 def index(request):
 
-    categories = Categories.objects.all()
-
     context = {
-        'title': 'Textura - Главная',
-        'content': 'Магазин тканей Textura',
-        'categories': categories
+        "title": "Textura - Главная",
+        "content": "Магазин тканей Textura",
     }
-    return render(request, 'main/index.html', context)
+    return render(request, "main/index.html", context)
+
 
 def about(request):
     context = {
-        'title': 'Textura - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Добро пожаловать в интернет-магазин Textura — место, где стиль, комфорт и качество встречаются для создания идеального пространства!',
+        "title": "Textura - О нас",
+        "content": "О нас",
+        "text_on_page": "Добро пожаловать в интернет-магазин Textura — место, где стиль, комфорт и качество встречаются для создания идеального пространства!",
     }
-    return render(request, 'main/about.html', context)
+    return render(request, "main/about.html", context)
